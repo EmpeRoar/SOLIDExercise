c10feb693c39b721d9e3f29a6f554a3b344f84d6
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-25 06:45:25 +0800

    SOLID Init

--- SOLIDExercise/Program.cs
+++ SOLIDExercise/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
-namespace SOLIDExercise
+namespace OCP
 {
+   
+
+    public interface ITurtle
+    {
+        
+    }
+    public class Turtle : ITurtle
+    {
+        public int XPos { get; set; } = 0;
+        public int YPos { get; set; } = 0;
+        public string Face { get; set; } = "NORTH";
+    }
+
+    public static class TurtleExt
+    {
+        public static Turtle Move(this Turtle turle)
+        {
+            turle.XPos++;
+            return turle;
+        }
+
+        public static void Report(this Turtle turle)
+        {
+            Console.WriteLine($"Report: {turle.XPos} {turle.YPos} {turle.Face}");
+            
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var turtle = new Turtle();
+            turtle.Move();
+            turtle.Move();
+            turtle.Move();
+            turtle.Move();
+            turtle.Report();
+            Console.ReadLine();
         }
     }
 }
