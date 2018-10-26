77249b76111a4b481a8fe9ac7a18dae3bb0946a9
Julius Bacosa <juliusbacosa@gmail.com>
2018-10-27 07:02:23 +0800

    OCP

--- Payroll/Program.cs
+++ Payroll/Program.cs
@@ -10,6 +10,8 @@ namespace Payroll
         {
             Employee driver = new Driver();
             driver.CurrentSalary = 300;
+            driver.FullName = "Julius Bacosa";
+            
 
             Calculator calculator = new Calculator();
             var BasicCalculation = calculator.Execute(driver, new BasicCalculation());
@@ -17,7 +19,9 @@ namespace Payroll
             var OvertimeCalculation = calculator.Execute(driver, new OvertimeCalculation());
             var HazardPayCalculation = calculator.Execute(driver, new HazardPayCalculation());
 
-            Console.WriteLine($"P{BasicCalculation}: P{HolidayCalculation}: P{OvertimeCalculation}: P{HazardPayCalculation}");
+            driver.TotalPay = BasicCalculation + HolidayCalculation + OvertimeCalculation + HazardPayCalculation;
+
+            Console.WriteLine($"P{BasicCalculation}: P{HolidayCalculation}: P{OvertimeCalculation}: P{HazardPayCalculation}, Total Pay: {driver.TotalPay}");
 
             Console.ReadLine();
         }
